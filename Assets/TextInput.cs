68b03c45cdf8f7aa4045e0ca92e8d8d297c4632a
Aaron Votre <avotre422@gmail.com>
2019-02-14 21:55:58 -0500

    Listen to keyboard and log keystrokes

---
+++ Assets/TextInput.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInput : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (char letter in Input.inputString) {
+            Debug.Log(letter);
+        }
+	}
+}
